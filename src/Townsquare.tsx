c392728daa2c943d07c90820bdd9e2f6ef3402c7
Matthieu <matthieu.jeanson@gmail.com>
2024-07-06 15:54:24 -0700

    Put a placeholder for the tapPlayer function for now

--- src/Townsquare.tsx
+++ src/Townsquare.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import PlayerToken from './PlayerToken.tsx';
-import { useAppSelector  } from './hooks.ts';
+import { useAppSelector } from './hooks.ts';
 
 type TownsquareProps = {}
 
 function Townsquare(props: TownsquareProps) {
     const players = useAppSelector(state => state.players.players)
     return <ul>
-        { players.map((p,index) => <PlayerToken key={p.id} index={index} />) }
+        {players.map((p, index) => <PlayerToken key={p.id} index={index}
+            tapPlayer={() => { }} />)}
     </ul>
 }
 
