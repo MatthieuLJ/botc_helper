a28b74aa38546a048cb9265ac6f60deacca72437
Matthieu <matthieu.jeanson@gmail.com>
2024-07-08 20:43:28 -0700

    Setup routes system

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
+import React from 'react';
 import './App.css';
 import TownsquareSetup from './TownsquareSetup.tsx';
+import Townsquare from './Townsquare.tsx';
 import GameState from './game/GameState.tsx';
 import { Provider } from 'react-redux';
-import { ScriptProvider } from './game/ScriptContext.tsx'
+import { ScriptProvider } from './game/ScriptContext.tsx';
+import { createBrowserRouter, RouterProvider, Navigate } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/setup" replace />,
+  },
+  {
+    path: "/setup",
+    element: <TownsquareSetup />,
+  },
+  {
+    path: "/play",
+    element: <Townsquare />
+  }
+]);
 
 function App() {
   return (
-    <ScriptProvider>
-      <Provider store={GameState}>
-        <div className="App">
-          <TownsquareSetup />
-        </div>
-      </Provider>
-    </ScriptProvider>
+    <React.StrictMode>
+      <ScriptProvider>
+        <Provider store={GameState}>
+          <div className="App">
+            <RouterProvider router={router} />
+          </div>
+        </Provider>
+      </ScriptProvider>
+    </React.StrictMode>
   );
 }
 
