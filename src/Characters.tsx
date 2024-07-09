8daeaf24faadbccafe84d7e4a668ca031e148dfe
Matthieu <matthieu.jeanson@gmail.com>
2024-07-08 23:20:00 -0700

    Enable highlighting of characters

--- src/Characters.tsx
+++ src/Characters.tsx
@@ -3,17 +3,29 @@ import { ScriptContext, ScriptContextType } from './game/ScriptContext.tsx';
 import { roleType } from "./game/role.ts";
 
 type CharactersPropType = {
-    highlights: string[] 
+    highlights: string[];
 } | null;
 
-function Characters (props: CharactersPropType) {
+function Characters(props: CharactersPropType) {
     const { roles, getRole, }: ScriptContextType = useContext(ScriptContext);
 
+
+    const highlights: string[] = props?.highlights ?? [];
+    const role_infos: [string, roleType, boolean][] = [];
+    roles.forEach((role) => {
+        const role_info: roleType | null = getRole(role);
+        if (role_info != null) {
+            role_infos.push([role, role_info, highlights.includes(role)]);
+        }
+    });
+
     return <ul>
-        {roles.map((role) => {
-            const role_info: roleType | null = getRole(role);
-            if (role_info == null) return null;
-            return <li key={role}>{role_info.name}</li>;
+        {role_infos.map((role) => {
+            return <li key={role[0]}>
+                <p style={{ fontWeight: role[2] ? "bold" : "normal" }}>
+                    {role[1].name}
+                </p>
+            </li>;
         })}
     </ul>;
 };
