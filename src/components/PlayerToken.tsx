51811c7a82b1755970cb4b0adf53265982d6c15f
Matthieu <matthieu.jeanson@gmail.com>
2024-08-20 06:22:11 -0700

    Show roles in Townsquare (need more styling and alignment though)

--- src/components/PlayerToken.tsx
+++ src/components/PlayerToken.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useAppSelector } from '../state/hooks.ts';
+import { Avatar, AvatarGroup, Box } from '@mui/material';
+import { ScriptContext, ScriptContextType } from '../state/ScriptContext.tsx';
+
+import token_background from '../img/token_background.png';
 
 type PlayerTokenProps = {
     index: number,
-    tapPlayer: (index: number) => void
-}
+    tapPlayer: (index: number) => void;
+};
 
 function PlayerToken(props: PlayerTokenProps) {
     const player_info = useAppSelector(state => state.players.players[props.index]);
+    const { getRole }: ScriptContextType = useContext(ScriptContext);
 
     return <li onClick={() => props.tapPlayer(props.index)}>
-        {player_info.name}
+        <Box>
+            <div>
+                <AvatarGroup max={5}>
+                    { player_info.claims.length === 0 ?
+                    <Avatar alt="No claim" src={token_background} />
+                    :
+                    player_info.claims.map((c) => {
+                        const role_info = getRole(c);
+                        return <Avatar alt={c} src={role_info?.icon}/>
+                    })}
+                </AvatarGroup>
+            </div>
+            <div>{player_info.name}</div>
+        </Box>
+
     </li>;
 }
 
