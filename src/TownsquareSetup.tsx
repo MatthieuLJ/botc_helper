cdc61aeaac2a4c75a3047b09dea9c33995d23913
Matthieu <matthieu.jeanson@gmail.com>
2024-09-02 00:43:33 -0700

    Use Townsquare for setup

--- src/TownsquareSetup.tsx
+++ src/TownsquareSetup.tsx
@@ -6,6 +6,7 @@ import Characters from './components/Characters.tsx';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from './state/hooks.ts';
 import { advanceTime } from './state/TimeSlice.tsx';
+import { Button, Dialog } from '@mui/material';
 
 function TownsquareSetup() {
     const { rolesLoading }: ScriptContextType = useContext(ScriptContext);
@@ -14,28 +15,33 @@ function TownsquareSetup() {
     const navigate = useNavigate();
     const dispatch = useAppDispatch();
 
-    return <>
-        <div>
+    return <div className="h-screen">
+        <div className="h-2/3">
             <PlayersSetup />
+        </div>
+        <div className="h-1/3">
             <ScriptSetup />
             <button onClick={() => { setShowCharacters(true); }}
                 disabled={rolesLoading || roles.length === 0}>Show script</button>
-            <dialog open={showCharacters}
-                onClose={() => { setShowCharacters(false); }}>
-                <Characters
-                    highlights={null}
-                    closeDialog={() => { setShowCharacters(false); }}
-                    tapCharacter={() => {/* could show ability here*/ }} />
-            </dialog>
+
             <p>
-                <button onClick={() => {
+                <Button onClick={() => {
                     dispatch(advanceTime());
                     return navigate('/play/townsquare', { replace: true });
                 }}
-                    disabled={rolesLoading || roles.length === 0}>Done</button>
+                    disabled={rolesLoading || roles.length === 0}>
+                    Done
+                </Button>
             </p>
         </div>
-    </>;
+        <Dialog open={showCharacters}
+            onClose={() => { setShowCharacters(false); }}>
+            <Characters
+                highlights={null}
+                closeDialog={() => { setShowCharacters(false); }}
+                tapCharacter={() => {/* could show ability here*/ }} />
+        </Dialog>
+    </div>;
 }
 
 export default TownsquareSetup;
--- src/components/PlayersSetup.tsx
+++ src/components/PlayersSetup.tsx
@@ -4,11 +4,13 @@ import Counter from "./Counter.tsx";
 import PlayerToken from "./PlayerToken.tsx";
 import { useAppDispatch, useAppSelector } from "../state/hooks.ts";
 import { setName } from '../state/PlayersSlice.tsx';
+import { Button, Dialog, TextField } from "@mui/material";
+import Townsquare from "../Townsquare.tsx";
 
 function PlayersSetup(props) {
     const players = useAppSelector(state => state.players.players);
     const dispatch = useAppDispatch();
-    
+
     // To be able to set the player name during setup
     const [changingName, setChangingName] = useState(false);
     const [nameField, setNameField] = useState("");
@@ -19,26 +21,29 @@ function PlayersSetup(props) {
         setChangingName(false);
     };
 
-    return <>
-        <Counter />
-        <ul>
-            {players.map((p, index) => <PlayerToken key={p.id} index={index}
-                tapPlayer={() => {
+    return <div className="flex flex-col h-full">
+        <div className="flex-none h-fit">
+            <Counter />
+        </div>
+        <div className="flex w-full flex-grow place-content-center max-h-full">
+            <div className="aspect-square max-h-full max-w-full">
+                <Townsquare tapAction={function (index: number): void {
                     setChangingName(true);
                     setCurrentIndex(index);
                     setNameField(players[index].name);
-                }} />)}
-        </ul>
-        <dialog open={changingName} onClose={handleClose}>
+                }} />
+            </div>
+        </div>
+        <Dialog open={changingName} onClose={handleClose}>
             <form method="dialog">
                 <p>Set the player's name</p>
-                <input name="name_field" id="player_name_field" type="text" value={nameField}
+                <TextField name="name_field" id="player_name_field" type="text" value={nameField}
                     onChange={(e) => setNameField(e.currentTarget.value)} />
-                <button onClick={acceptNewName}>OK</button>
-                <button onClick={() => { setNameField(""); setChangingName(false); }}>Cancel</button>
+                <Button onClick={acceptNewName}>OK</Button>
+                <Button onClick={() => { setNameField(""); setChangingName(false); }}>Cancel</Button>
             </form>
-        </dialog>
-    </>;
+        </Dialog>
+    </div>;
 }
 
 export { PlayersSetup };
