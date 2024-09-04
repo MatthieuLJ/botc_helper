83f9fd68afeaac860c77affa244fa12a4f38502f
Matthieu <matthieu.jeanson@gmail.com>
2024-09-03 21:34:31 -0700

    Also hide notes

--- src/components/NoteChip.tsx
+++ src/components/NoteChip.tsx
@@ -10,6 +10,7 @@ import Chip from "@mui/material/Chip";
 
 import { mdiScriptTextOutline, mdiFaceManOutline, mdiWeatherNight, mdiWeatherSunny } from '@mdi/js';
 import Icon from '@mdi/react';
+import { PlayContext, PlayContextType } from "../state/PlayContext.tsx";
 
 
 type PlayerChipProps = {
@@ -26,13 +27,30 @@ function PlayerChip(props: PlayerChipProps) {
     // https://redux.js.org/usage/deriving-data-selectors#optimizing-selectors-with-memoization [object Object] 
     const players = useAppSelector(
         state => state.players.players);
+    const { hideInformation }: PlayContextType = useContext(PlayContext);
+    var token_sx = { sx: {} };
+    if (hideInformation) {
+        token_sx = {
+            sx: {
+                ...token_sx['sx'],
+                '& .MuiChip-label': {
+                    color: "transparent",
+                    textShadow: "0 0 5px rgba(0,0,0,0.5)",
+                },
+                '& .MuiChip-avatar': {
+                    filter: "blur(5px)"
+                }
+            }
+        };
+    }
     if (props.index < 0) {
         return <Chip
             label="player"
             avatar={<Icon path={mdiFaceManOutline} />}
             variant="outlined"
             {...(props.onDelete ? { onDelete: props.onDelete } : {})}
-            {...(props.onClick ? { onClick: props.onClick } : {})} />;
+            {...(props.onClick ? { onClick: props.onClick } : {})}
+            {...token_sx} />;
     } else {
         const player = players[props.index];
         return <Chip
@@ -40,7 +58,8 @@ function PlayerChip(props: PlayerChipProps) {
             avatar={<Icon path={mdiFaceManOutline} />}
             variant="outlined"
             {...(props.onDelete ? { onDelete: props.onDelete } : {})}
-            {...(props.onClick ? { onClick: props.onClick } : {})} />;
+            {...(props.onClick ? { onClick: props.onClick } : {})}
+            {...token_sx} />;
     }
 }
 
@@ -52,22 +71,39 @@ type RoleChipProps = {
 
 function RoleChip(props: RoleChipProps) {
     const { getRole }: ScriptContextType = useContext(ScriptContext);
-
     const role_info: roleType | null = getRole(props.role);
+    const { hideInformation }: PlayContextType = useContext(PlayContext);
+    var token_sx = { sx: {} };
+    if (hideInformation) {
+        token_sx = {
+            sx: {
+                ...token_sx['sx'],
+                '& .MuiChip-label': {
+                    color: "transparent",
+                    textShadow: "0 0 5px rgba(0,0,0,0.5)",
+                },
+                '& .MuiChip-avatar': {
+                    filter: "blur(5px)"
+                }
+            }
+        };
+    }
     if (role_info != null) {
         return <Chip
             label={role_info.name}
             avatar={<Avatar alt="" src={role_info.icon} />}
             variant="outlined"
             {...(props.onDelete ? { onDelete: props.onDelete } : {})}
-            {...(props.onClick ? { onClick: props.onClick } : {})} />;
+            {...(props.onClick ? { onClick: props.onClick } : {})}
+            {...token_sx} />;
     } else {
         return <Chip
             label="role"
             avatar={<Icon path={mdiScriptTextOutline} />}
             variant="outlined"
             {...(props.onDelete ? { onDelete: props.onDelete } : {})}
-            {...(props.onClick ? { onClick: props.onClick } : {})} />;
+            {...(props.onClick ? { onClick: props.onClick } : {})}
+            {...token_sx} />;
     }
 }
 
@@ -79,18 +115,36 @@ type TimeChipProps = {
 
 function TimeChip(props: TimeChipProps) {
     const night: boolean = (props.time % 2 === 0);
+    const { hideInformation }: PlayContextType = useContext(PlayContext);
+    var token_sx = { sx: {} };
+    if (hideInformation) {
+        token_sx = {
+            sx: {
+                ...token_sx['sx'],
+                '& .MuiChip-label': {
+                    color: "transparent",
+                    textShadow: "0 0 5px rgba(0,0,0,0.5)",
+                },
+                '& .MuiChip-avatar': {
+                    filter: "blur(5px)"
+                }
+            }
+        };
+    }
     if (night) {
         return <Chip label={"night " + Math.floor((props.time / 2) + 1)}
             avatar={<Icon path={mdiWeatherNight} />}
             variant="outlined"
             {...(props.onDelete ? { onDelete: props.onDelete } : {})}
-            {...(props.onClick ? { onClick: props.onClick } : {})} />;
+            {...(props.onClick ? { onClick: props.onClick } : {})}
+            {...token_sx} />;
     } else {
         return <Chip label={"day " + Math.floor((props.time / 2) + 1)}
             avatar={<Icon path={mdiWeatherSunny} />}
             variant="outlined"
             {...(props.onDelete ? { onDelete: props.onDelete } : {})}
-            {...(props.onClick ? { onClick: props.onClick } : {})} />;
+            {...(props.onClick ? { onClick: props.onClick } : {})}
+            {...token_sx} />;
     }
 }
 
--- src/components/NoteDisplay.tsx
+++ src/components/NoteDisplay.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Box from "@mui/material/Box";
 
 import NoteChip from "./NoteChip.tsx";
 import { NoteSegments } from "../state/NotesSlice.tsx";
+import { PlayContext, PlayContextType } from "../state/PlayContext.tsx";
 
 type NoteDisplayProps = {
     content: NoteSegments;
 };
 
 export default function NoteDisplay(props: NoteDisplayProps) {
+    const { hideInformation }: PlayContextType = useContext(PlayContext);
     return (
         <Box>
             {props.content.map((item, index) => {
@@ -22,7 +24,16 @@ export default function NoteDisplay(props: NoteDisplayProps) {
                 } else {
                     return (
                         <span
-                            key={index}>
+                            key={index}
+                            {...(hideInformation ?
+                                {
+                                    style: {
+                                        color: "transparent",
+                                        textShadow: "0 0 5px rgba(0,0,0,0.5)"
+                                    }
+                                }
+                                : {})}
+                        >
                             {item}
                         </span>
                     );
