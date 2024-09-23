a4154d5a454d9dba00f40f03e1ac9d99dc12cdad
Matthieu <matthieu.jeanson@gmail.com>
2024-09-22 20:51:55 -0700

    Add function to move player

--- src/state/PlayersSlice.tsx
+++ src/state/PlayersSlice.tsx
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { WritableDraft } from 'immer';
 
 export type PlayerInfo = {
     name: string,
@@ -44,6 +45,47 @@ export const PlayersSlice = createSlice({
         setAlive: (state, action) => {
             state.players[action.payload.index].alive = action.payload.alive;
         },
+        movePlayer: (state, action) => {
+            const num_players = state.players.length;
+            let from_index = action.payload.from;
+            let to_index = action.payload.to;
+            if (to_index == from_index)
+                return;
+
+            // clockwise means the one token moves clockwise
+            // all tokens in between move in the opposite
+            // direction
+            const clockwise = (num_players + from_index - to_index) % num_players
+                > num_players / 2;
+
+            if (clockwise) {
+                // reverse to treat all cases as one and reverse back at the end
+                state.players.reverse();
+                from_index = num_players - 1 - from_index;
+                to_index = num_players - 1 - to_index;
+            }
+
+            if (from_index < to_index) {
+                // the end of the array needs to shift to the start
+                state.players.splice(0, 0,
+                    state.players.pop() as WritableDraft<PlayerInfo>);
+                from_index++;
+                to_index++;
+            }
+
+            const moving_player: PlayerInfo = state.players[from_index];
+            state.players.splice(from_index, 1);
+            if (to_index > from_index) {
+                to_index = (num_players + to_index - 1) % num_players;
+            }
+            state.players.splice(to_index, 0, moving_player);
+
+            if (clockwise) {
+                // reverse back
+                state.players.reverse();
+            }
+
+        },
         resetPlayers: (state) => {
             state.players.forEach((p) => {
                 p.alive = true;
@@ -53,6 +95,6 @@ export const PlayersSlice = createSlice({
     }
 });
 
-export const { setCount, setName, setClaims, setAlive, resetPlayers } = PlayersSlice.actions;
+export const { setCount, setName, setClaims, setAlive, resetPlayers, movePlayer } = PlayersSlice.actions;
 
 export default PlayersSlice.reducer;
